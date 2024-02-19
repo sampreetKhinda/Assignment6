610cefbd7f15c6b0b873a09743bc38bd00b92170
Sampreet <sampreetsingh0903@gmail.com>
2024-02-19 11:31:34 -0500

    Initial Commit

---
+++ Assignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment6
+//
+//  Created by Sampreet singh on 19/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
